88fb0fc6e7e16d6daa52a923d61c6641244ef4ee
marian-kowall_pace <marian.kowall@cariad.technology>
2025-10-13 13:26:11 +0200

    add gitgnore and and zed scripts and new zed config file

--- work_scripts/copy_config_from_repo_to_host.sh
+++ work_scripts/copy_config_from_repo_to_host.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+rsync -av $HOME/repo/config_file/linux/.config/zed/settings.json $HOME/.config/zed/settings.json
+rsync -av $HOME/repo/config_file/linux/.config/zed/keymap.json $HOME/.config/zed/keymap.json
+
 rsync -av $HOME/repo/config_file/linux/.config/.myshellconfig.sh $HOME/.config/.myshellconfig.sh
 
 ZSHRC="$HOME/.zshrc"
