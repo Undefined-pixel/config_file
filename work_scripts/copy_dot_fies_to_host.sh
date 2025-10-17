4295e5823e9be3cf9ed162ab337eadbe31e63c8f
marian-kowall_pace <marian.kowall@cariad.technology>
2025-10-17 18:08:47 +0200

    new stuff

--- work_scripts/copy_dot_fies_to_host.sh
+++ work_scripts/copy_dot_fies_to_host.sh
@@ -2,10 +2,11 @@
 
 source ./helper_functions.sh
 
-copy_vimrc_to_home() {
+copy_dot_to_home() {
   rsync -av .vimrc $HOME/.vimrc
+  rsync -av .tmux.conf $HOME/.tmux.conf
 }
 
 move_to_pre_config_folder
-copy_vimrc_to_home
+copy_dot_to_home
 go_back_to_workpace "$path"
