01a9f21826eb3296563a28338c56554f091082bb
mUndefined-pixel <mariankowall164@gmail.de>
2025-10-28 19:33:23 +0100

    new stuff

--- work_scripts/copy_config_from_repo_to_host.sh
+++ work_scripts/copy_config_from_repo_to_host.sh
@@ -2,8 +2,8 @@
 
 rsync -av $HOME/repo/config_file/linux/.config/zed/settings.json $HOME/.config/zed/settings.json
 rsync -av $HOME/repo/config_file/linux/.config/zed/keymap.json $HOME/.config/zed/keymap.json
-
 rsync -av $HOME/repo/config_file/linux/.config/.myshellconfig.sh $HOME/.config/.myshellconfig.sh
+rsync -av $HOME/repo/config_file/linux/.config/fastfetch/ $HOME/.config/fastfetch
 
 ZSHRC="$HOME/.zshrc"
 LINE='source ~/.config/.myshellconfig.sh'
--- work_scripts/copy_dot_fies_to_host.sh
+++ work_scripts/copy_dot_fies_to_host.sh
@@ -5,6 +5,10 @@ source ./helper_functions.sh
 copy_dot_to_home() {
   rsync -av .vimrc $HOME/.vimrc
   rsync -av .tmux.conf $HOME/.tmux.conf
+  OS=$(uname)
+  if [[ "$OS" == "Darwin" ]]; then
+    rsync -av .aerospace.toml $HOME/.aerospace.toml
+  fi
 }
 
 move_to_pre_config_folder
